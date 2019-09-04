14693ae8598b64edd58d7d651eda0b077b06709f
Michael <sound.plus.fury@gmail.com>
2019-09-04 13:57:14 -0700

    fixes

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const signupLimiter = new RateLimit({
 	message: "Maximum accounts created, please try again later. Glad you like our site so much tho..."
 });
 
-mongoose.connect('mongodb://localhost/budwiser', {useNewUrlParser: true});
+mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true});
 const db = mongoose.connection;
 db.once('open', () => {
 	console.log(`Connected to Mongo on ${db.host}:${db.port}`);
@@ -38,7 +38,8 @@ db.on('error', (err) => {
 
 app.use('/auth', require('./routes/auth'));
 app.use('/api', expressJWT({secret: process.env.JWT_SECRET}), require('./routes/database'));
-// //       ^^where it's from       ^^where it goes
+app.use(express.static(__dirname + '/client/build'));
+app.get('*', function(req, res) {res.sendFile(__dirname + '/client/build/index.html');});
 
 app.listen(process.env.PORT, () => {
 	console.log(` 🎧"You're listening to K-Billy Super Sounds of the 70's on port ${process.env.PORT}..."🎧 `);
